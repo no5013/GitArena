d20a12eba836eac6395a14f1a96dd1061681c9a0
Pongsakorn Somsri <pongsakorns@gmail.com>
2017-06-07 17:29:54 +0900

    Add initial server

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var app = express();
+
+var port = process.env.PORT || 7777
+
+app.get('/', function (req, res) {
+    res.send('<h1>Hello Node.js</h1>');
+});
+
+app.get('/index', function (req, res) {
+    res.send('<h1>This is index page</h1>');
+});
+
+app.post('/newuser', function (req, res) {
+    var json = req.body;
+    res.send('Add new ' + json.name + ' Completed!');
+});
+
+app.listen(port, function() {
+    console.log('Starting node.js on port ' + port);
+});
