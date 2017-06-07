ec021d404e69ccfbd34045023bb0b74d8781e25f
Pongsakorn Somsri <pongsakorns@gmail.com>
2017-06-07 18:38:02 +0900

    Create rest api for get data from git

---
+++ get-repo.js
@@ -0,0 +1,50 @@
+/**
+* Get GitHub user informations
+*/
+
+const GitHubClient = require('./libs/GitHubClient.js').GitHubClient;
+const users = require('./libs/features/users');
+const octocat = require('./libs/features/octocat')
+const repo = require('./libs/features/repositories')
+const commit = require('./libs/features/commits')
+const stat = require('./libs/features/stats')
+
+function GitGetter() {
+
+  this.getUserData = function(userName, callback){
+    var data = {}
+
+    let githubCli = new GitHubClient({
+      baseUri:"https://api.github.com",
+      token: process.env.TOKEN_GITHUB_DOT_COM
+    }, users, octocat, repo, commit, stat);
+
+    githubCli.fetchUserRepositories({handle: userName})
+    .then(repos => {
+      n = 10
+      repo_name = repos[n]['name']
+      login = repos[n]['owner']['login']
+      data['repo_name'] = repo_name
+      // githubCli.fetchUser({handle: login})
+      //   .then(owner => {
+      //   console.log(owner);
+      //   })
+      //   .catch(error => {
+      //     console.log("error", error)
+      //   });
+      // githubCli.fetchAllCommit({owner: login, repository: repos[0]['name']})
+      //   .then(commits => {
+      //     console.log(commits[0])
+      //   })
+      githubCli.fetchUserStats({owner: login, repository: repo_name})
+      .then(stats => {
+        console.log(stats[0])
+        data['stat'] = stats[0]
+      })
+
+      callback(data)
+    })
+  }
+}
+
+module.exports = new GitGetter();
--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 var express = require('express')
 var app = express();
-
 var port = process.env.PORT || 7777
 
-app.get('/', function (req, res) {
-    res.send('<h1>Hello Node.js</h1>');
-});
+var bodyParser = require('body-parser');
 
-app.get('/index', function (req, res) {
-    res.send('<h1>This is index page</h1>');
+var gitRepo = require('./get-repo')
+
+// parse application/json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', function (req, res) {
+  gitRepo.getUserData("b5710546232", function(result){
+      res.send(result);
+  })
 });
 
 app.post('/newuser', function (req, res) {
     var json = req.body;
-    res.send('Add new ' + json.name + ' Completed!');
+    gitRepo.getUserData(json.name, function(result){
+        res.send(result);
+    })
 });
 
 app.listen(port, function() {
