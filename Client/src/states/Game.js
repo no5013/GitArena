ce3cee7a0ba20fc1b195942b302f77236263c650
Pongsakorn Somsri <pongsakorns@gmail.com>
2017-06-14 13:35:22 +0900

    Refactor init code

--- Client/src/states/Game.js
+++ Client/src/states/Game.js
@@ -10,6 +10,16 @@ var layer;
 var map;
 var moving = {}
 var players = []
+var spawn_points = [
+  {
+    x: 10,
+    y: 10
+  },
+  {
+    x: 10,
+    y: 12
+  }
+]
 
 export default class extends Phaser.State {
 
@@ -23,77 +33,16 @@ export default class extends Phaser.State {
   }
 
   create () {
-    var spawn_points = [
-      {
-        x: 10,
-        y: 10
-      },
-      {
-        x: 10,
-        y: 12
-      }
-    ]
+    this.initMap()
 
-    var data = '';
-    var size_x = 30;
-    var size_y = 30;
-    for (var y = 0; y < size_y; y++)
-    {
-      for (var x = 0; x < size_x; x++)
-      {
-        // data += game.rnd.between(0, 47).toString();
+    this.initMarker()
 
-        data += "0"
-
-        if (x < size_x-1)
-        {
-          data += ',';
-        }
-      }
-
-      if (y < size_y-1)
-      {
-        data += "\n";
-      }
-    }
-    game.cache.addTilemap('dynamicMap', null, data, Phaser.Tilemap.CSV);
-    //  Create our map (the 16x16 is the tile size)
-    map = game.add.tilemap('dynamicMap', 32, 32);
-    //  'tiles' = cache image key, 16x16 = tile size
-    map.addTilesetImage('tiles', 'tiles', 32, 32);
-    //  0 is important
-    layer = map.createLayer(0);
-    //  Scroll it
-    layer.resizeWorld();
-
-    // mouse input
-    marker = game.add.graphics();
-    marker.lineStyle(2, 0xffffff, 1);
-    marker.drawRect(0, 0, 32, 32);
-    game.input.addMoveCallback(this.updateMarker, this);
-    game.input.onDown.add(this.getTileProperties, this);
+    this.initCharacters()
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     cursors = game.input.keyboard.createCursorKeys();
     this.cursors = cursors
-
-    let self = this
-    let runner = 0
-    spawn_points.forEach(function(spawn_point){
-      players.push(new RepoHero({
-        game: self,
-        x: spawn_points[runner].x*32,
-        y: spawn_points[runner].y*32,
-        asset: 'chara',
-        name: self.game.repos[runner].repo_name
-      }))
-      self.game.add.existing(players[runner])
-      let tile = map.getTile(spawn_points[runner].x, spawn_points[runner].y)
-      tile.properties['owner'] = players[runner++];
-    })
-
-
   }
 
   update () {
@@ -151,7 +100,7 @@ export default class extends Phaser.State {
     }
     else if(moving['isSelected']){
       if(tile===moving['fromTile'])
-        return;
+      return;
       this.moveCharacter(moving['character'], x, y)
       tile.properties['owner'] = moving['character']
       moving['fromTile'].properties['owner'] = null
@@ -168,57 +117,66 @@ export default class extends Phaser.State {
     marker.x = layer.getTileX(game.input.activePointer.worldX) * 32;
     marker.y = layer.getTileY(game.input.activePointer.worldY) * 32;
   }
-  // moveCharacter(sprite, cell_x, cell_y) {
-  //   let currenctCell_x = sprite.x/32
-  //   let currenctCell_y = sprite.y/32
-  //
-  //   let diff_x = Math.abs(currenctCell_x - cell_x)
-  //   let diff_y = Math.abs(currenctCell_y - cell_y)
-  //
-  //   let speed = 200
-  //
-  //   console.log(diff_y)
-  //
-  //   if(game.isMoving)
-  //     return false;
-  //   game.isMoving = true;
-  //
-  //   if (diff_x == 0){
-  //     var characterMovement_vertical = game.add.tween(sprite);
-  //     characterMovement_vertical.to({y: cell_y*32},diff_y*speed);
-  //     characterMovement_vertical.onComplete.add(function(){
-  //       game.isMoving = false
-  //     }, this)
-  //     characterMovement_vertical.start();
-  //   }
-  //
-  //   else if (diff_y == 0){
-  //     var characterMovement_horizontal = game.add.tween(sprite);
-  //     var characterMovement_vertical = game.add.tween(sprite);
-  //
-  //     characterMovement_horizontal.to({x: cell_x*32},diff_x*speed);
-  //     characterMovement_horizontal.onComplete.add(function(){
-  //       game.isMoving = false
-  //     }, this)
-  //     characterMovement_horizontal.start();
-  //   }
-  //   else {
-  //     var characterMovement_horizontal = game.add.tween(sprite);
-  //     var characterMovement_vertical = game.add.tween(sprite);
-  //
-  //     characterMovement_horizontal.to({x: cell_x*32},diff_x*speed);
-  //     characterMovement_horizontal.onComplete.add(function(){
-  //       characterMovement_vertical.start();
-  //     }, this)
-  //
-  //     characterMovement_vertical.to({y: cell_y*32},diff_y*speed);
-  //     characterMovement_vertical.onComplete.add(function(){
-  //       game.isMoving = false
-  //     }, this)
-  //
-  //     characterMovement_horizontal.start();
-  //   }
-  // }
+
+  initMarker() {
+    // mouse input
+    marker = game.add.graphics();
+    marker.lineStyle(2, 0xffffff, 1);
+    marker.drawRect(0, 0, 32, 32);
+    game.input.addMoveCallback(this.updateMarker, this);
+    game.input.onDown.add(this.getTileProperties, this);
+  }
+
+  initMap() {
+    var data = '';
+    var size_x = 30;
+    var size_y = 30;
+    for (var y = 0; y < size_y; y++)
+    {
+      for (var x = 0; x < size_x; x++)
+      {
+        // data += game.rnd.between(0, 47).toString();
+
+        data += "0"
+
+        if (x < size_x-1)
+        {
+          data += ',';
+        }
+      }
+
+      if (y < size_y-1)
+      {
+        data += "\n";
+      }
+    }
+    game.cache.addTilemap('dynamicMap', null, data, Phaser.Tilemap.CSV);
+    //  Create our map (the 16x16 is the tile size)
+    map = game.add.tilemap('dynamicMap', 32, 32);
+    //  'tiles' = cache image key, 16x16 = tile size
+    map.addTilesetImage('tiles', 'tiles', 32, 32);
+    //  0 is important
+    layer = map.createLayer(0);
+    //  Scroll it
+    layer.resizeWorld();
+  }
+
+  initCharacters() {
+    let self = this
+    let runner = 0
+    spawn_points.forEach(function(spawn_point){
+      players.push(new RepoHero({
+        game: self,
+        x: spawn_points[runner].x*32,
+        y: spawn_points[runner].y*32,
+        asset: 'chara',
+        name: self.game.repos[runner].repo_name
+      }))
+      self.game.add.existing(players[runner])
+      let tile = map.getTile(spawn_points[runner].x, spawn_points[runner].y)
+      tile.properties['owner'] = players[runner++];
+    })
+  }
 
   render () {
   }
