2eaced74af4f3dd2b3a255e185eecff1a2baf7f0
Pongsakorn Somsri <pongsakorns@gmail.com>
2017-07-07 16:33:55 +0900

    Create Main menu

--- Client/src/states/MainMenuState.js
+++ Client/src/states/MainMenuState.js
@@ -5,9 +5,25 @@ import Menu from '../prefabs/huds/Menu'
 
 import LevelMenuItem from '../prefabs/huds/LevelMenuItem'
 
+import StageSelectionMenuItem from '../prefabs/huds/MainMenuHuds/StageSelectionMenuItem'
+
+import MainMenuSelectionState from '../StateMachine/MainMenuState/MainMenuSelectionState'
+import StageSelectionState from '../StateMachine/MainMenuState/StageSelectionState'
+
 export default class extends Phaser.State {
 
   init () {
+
+    let self = this;
+    this.MainMenuState = {
+      MainMenuSelectionState: new MainMenuSelectionState(self),
+      StageSelectionState: new StageSelectionState(self)
+    }
+
+    this.properties = {
+      ActionStateVar: {}
+    }
+
     this.prefabs = {}
     this.groups = {
       hud: this.game.add.group()
@@ -21,25 +37,63 @@ export default class extends Phaser.State {
   create () {
     var self = this
 
+
     this.TEXT_STYLE = {font: "30px Arial", fill: "#FFFFFF"}
     this.HUD_TEXT_STYLE = {font: "16px Arial", fill: "#FFFFFF"}
-    // this.init_player_actions({x:400, y:100});
 
-    this.initActionMenu({x:400, y:100})
-    this.enableActionCommandHud()
+    // this.initActionMenu({x:400, y:100})
+    // this.enableActionCommandHud()
+    this.initMainMenu({x:400, y:100})
+    this.initStageSelectionMenu({x:400, y:100})
+
+    this.setMainMenuState(this.MainMenuState.MainMenuSelectionState)
   }
 
-  enableActionCommandHud(){
+  enableMainMenuHud(){
+    this.prefabs['main_menu'].enable();
+    this.prefabs['main_menu'].show();
+  }
+
+  disableMainMenuHud(){
+    this.prefabs['main_menu'].disable();
+    this.prefabs['main_menu'].hide();
+  }
+
+  enableStageSelectionMenuHud(){
     this.prefabs['levels_selection_menu'].enable();
     this.prefabs['levels_selection_menu'].show();
   }
 
-  disableActionCommandHud(){
+  disableStageSelectionMenuHud(){
     this.prefabs['levels_selection_menu'].disable();
     this.prefabs['levels_selection_menu'].hide();
   }
 
-  initActionMenu(position) {
+  initMainMenu (position) {
+    var self = this
+
+    var actions, actions_menu_items, action_index, actions_menu
+
+    // Available Action
+    actions = [
+      {text: "Stage Select", item_constructor: StageSelectionMenuItem.prototype.constructor},
+      // {text: "Skill", item_constructor: SkillMenuItem.prototype.constructor},
+      // {text: "Walk", item_constructor: WalkMenuItem.prototype.constructor},
+      // {text: "Endturn", item_constructor: EndTurnMenuItem.prototype.constructor}
+    ]
+    actions_menu_items = []
+    action_index = 0;
+
+    // Create a menu item for each action
+    actions.forEach(function (action) {
+      actions_menu_items.push(new action.item_constructor(this, action.text+"_menu_item", {x: position.x, y: position.y + action_index * 35}, {group: "hud", text: action.text, style: Object.create(self.TEXT_STYLE)}));
+      action_index++;
+    }, this);
+    this.actions_menu = new Menu(this, "main_menu", position, {group: "hud", menu_items: actions_menu_items})
+    this.disableMainMenuHud()
+  }
+
+  initStageSelectionMenu(position) {
     var self = this
 
     var actions, levels_selection_menu_items, action_index, levels_selection_menu
@@ -69,10 +123,19 @@ export default class extends Phaser.State {
 
     // Create a menu item for each action
     actions.forEach(function (action) {
-      levels_selection_menu_items.push(new LevelMenuItem(this, action.level_name+"_menu_item", {x: 400, y:100 + action_index * 35}, {group: "hud", level: action.level, text: action.level_name, style: Object.create(self.TEXT_STYLE)}));
+      levels_selection_menu_items.push(new LevelMenuItem(this, action.level_name+"_menu_item", {x: position.x, y: position.y + action_index * 35}, {group: "hud", level: action.level, text: action.level_name, style: Object.create(self.TEXT_STYLE)}));
       action_index++;
     }, this);
     this.levels_selection_menu = new Menu(this, "levels_selection_menu", position, {group: "hud", menu_items: levels_selection_menu_items})
+    this.disableStageSelectionMenuHud()
+  }
+
+  setMainMenuState(state) {
+    if(this.currentState){
+      this.currentState.leaveState();
+    }
+    this.currentState = state;
+    this.currentState.enterState();
   }
 
   render () {
