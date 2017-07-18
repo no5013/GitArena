ff9d65f310f49818e12e884926bad8f967053793
Pongsakorn Somsri <pongsakorns@gmail.com>
2017-07-18 17:21:49 +0900

    Set up update function

--- Server/get-repo.js
+++ Server/get-repo.js
@@ -32,28 +32,8 @@ function GitGetter() {
     githubCli.fetchUserRepositories({handle: username})
     .then(result => {
       repos = []
-      // var itemsProcessed = 0;
       result.forEach(function(repo, index, array){
-        // itemsProcessed++;
-        // console.log(itemsProcessed)
-        // githubCli.getRepositoryLanguages({owner: username, repository: repo['name']})
-        // .then(result2 => {
-        //   repoData = {
-        //     repo_name: repo['name'],
-        //     fork: repo['fork'],
-        //     stargazers_count: repo['stargazers_count'],
-        //     watchers_count: repo['watchers_count'],
-        //     language: result2,
-        //     open_issue_count: ['open_issue_count']
-        //   }
-        //   repos.push(repoData)
-        // })
-
-        // if(itemsProcessed === array.length) {
-        //   callback(repos);
-        // }
-
-        repoData = {
+        var repoData = {
           repo_name: repo['name'],
           fork: repo['fork'],
           stargazers_count: repo['stargazers_count'],
@@ -74,26 +54,62 @@ function GitGetter() {
       repos = []
       var repos_length = result.length
       result.forEach(function(repo, index, array){
-        repoData = {
+        var repoData = {
           repo_name: repo['name'],
           fork: repo['fork'],
           stargazers_count: repo['stargazers_count'],
           watchers_count: repo['watchers_count'],
           language: repo['language'],
-          open_issue_count: ['open_issue_count']
+          open_issue_count: repo['open_issues_count'],
+          updated_at: repo['updated_at'],
+          forks_count: repo['forks_count']
         }
+        console.log(repo)
 
+        // check if repo is not empty
         if(repo['language']!=null){
           githubCli.fetchRepoStats({owner: username, repository: repo['name']})
           .then(result => {
-            repos.push(result[0].total)
-            console.log(repos.length + " / " + repos_length)
-            if(repos.length == repos_length-1){
+            //find match collaborator
+            for(let i=0; i<result.length; i++){
+              if(result[i].author.login == username){
+
+                //format weeks
+                var a=0
+                var d=0
+                for(let j=0; j<result[i].weeks.length; j++){
+                  a += result[i].weeks[j].a
+                  d += result[i].weeks[j].d
+                }
+
+                repos.push({
+                  repo_name: repo['name'],
+                  updated_at: repo['updated_at'],
+                  stargazers_count: repo['stargazers_count'],
+                  watchers_count: repo['watchers_count'],
+                  language: repo['language'],
+                  open_issue_count: repo['open_issues_count'],
+                  forks_count: repo['forks_count'],
+                  commits_count: result[i].total,
+                  added_count: a,
+                  deleted_count: d
+                })
+                console.log(repo['name'] + " " + result[i].total)
+                break;
+              }
+
+              //if user doesn't collaborate in this repo
+              if(result.length == i+1){
+                repos_length-=1
+              }
+            }
+            // console.log(repos.length + " / " + repos_length)
+            if(repos.length == repos_length){
               callback(repos)
             }
           })
         }else{
-          console.log("ERROR")
+          console.log(repoData.repo_name + " ERROR")
           repos_length-=1
           if(repos.length == repos_length){
             callback(repos)
